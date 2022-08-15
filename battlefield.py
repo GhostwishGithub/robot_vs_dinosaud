fc0a5a2546818a851aad1d5b266ca98840c2554f
unknown <bensandlin2@gmail.com>
2022-08-15 14:11:20 -0500

    Got a lot of help from JJ. I think this is ready for a code review. Otherwise, it's completely functional and both dinosaur and robot have some random attributes.

--- battlefield.py
+++ battlefield.py
@@ -3,26 +3,30 @@ from robot import Robot
 import random
 
 class Battlefield:
-    def __init__(self, robot, dinosaur, winner):
-        robot = Robot('Dinosaur Killer 9000', random.randrange(100, 150), random.randrange(10-15))
-        dinosaur = Dinosaur('Roarface', random.randrange(100, 150), random.randrange(10-15))
-        winner = 'JJ'
+    def __init__(self):
+        health = random.randrange(99, 151)
+        attack = random.randrange(9, 16)
+        self.robot = Robot('Dinosaur Killer 9000', random.randrange(99, 151), random.randrange(9, 16))
+        self.dinosaur = Dinosaur('Roarface', random.randrange(99, 151), random.randrange(9, 16))
+        self.winner = 'JJ'
 
-    def run_game(self, robot, dinosaur, winner):
+    def run_game(self):
         print("Allright folks! Get your goofiest music on because we're battling robots and dinosaurs!")
-        print(f"In this corner we have {robot.name} with health of {robot.health} and an attack power of {robot.active_weapon}!")
-        print(f"And in this corner we have {dinosaur.name} with a health of {dinosaur.health} and an attack power of {dinosaur.attack_power}")
-        while robot.health > 0 and dinosaur.health > 0:
-            Battlefield.battle_phase
-        if robot.health <= 0:
-            winner = dinosaur.name
+        print(f"In this corner we have {self.robot.name} with health of {self.robot.health} and an attack power of {self.robot.active_weapon}!")
+        print(f"And in this corner we have {self.dinosaur.name} with a health of {self.dinosaur.health} and an attack power of {self.dinosaur.attack_power}")
+        while self.robot.health > 0 and self.dinosaur.health > 0:
+            self.battle_phase()
+        if self.robot.health <= 0 and self.dinosaur.health > 0:
+            self.winner = self.dinosaur.name
+        elif self.robot.health > 0 and self.dinosaur.health <= 0:
+            self.winner = self.robot.name
         else:
-            winner = robot.name
-        Battlefield.display_winner
+            print("Holy crap they knocked each other out!")
+        self.display_winner()
 
-    def battle_phase(self, robot, dinosaur):
-        robot.attack(dinosaur)
-        dinosaur.attack(robot)
+    def battle_phase(self):
+        self.robot.attack(self.dinosaur)
+        self.dinosaur.attack(self.robot)
 
-    def display_winner(self, winner):
-        print(f"And the winner is! {self.winner}!")
+    def display_winner(self):
+        print(f"And the winner is: {self.winner}!")
